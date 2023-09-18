fd10c9852e446268f5965e09f30d39a86af7e115
YaroslawMus <yaroslawmusato@gmail.com>
2023-09-18 04:17:17 +0300

    Обновление

--- src/api/gifts.js
+++ src/api/gifts.js
@@ -5,37 +5,30 @@ import { processXMLData } from '../formort/xmlProcessor.js';
 import { mergePricesWithProductCard } from '../formort/mergePricesWithProductCard.js';
 import config from '../../config.js';
 
+// Создаем функцию для выполнения HTTP-запросов
+async function fetchData(url) {
+  const response = await axios.get(url);
+  const xmlData = response.data;
+  const resultData = await parseXML(xmlData);
+  const processedData = processXMLData(resultData);
+  return processedData;
+}
+
+// Главная функция для получения данных из API
 export async function fetchApiGiftData() {
-  let item = {};
   try {
-    const response = await axios.get(
-      `https://${config.GIFTS_API_KEY}@api2.gifts.ru/export/v2/catalogue/product.xml`,
-    );
-    const responseStock = await axios.get(
-      `https://${config.GIFTS_API_KEY}@api2.gifts.ru/export/v2/catalogue/stock.xml`,
-    );
-    const xmlData = response.data;
-    const resultData = await parseXML(xmlData);
-    const processedData = processXMLData(resultData);
+    // Параллельно получаем данные из двух источников
+    const [jsonData, jsonDataStock] = await Promise.all([
+      fetchData(`https://${config.GIFTS_API_KEY}@api2.gifts.ru/export/v2/catalogue/product.xml`),
+      fetchData(`https://${config.GIFTS_API_KEY}@api2.gifts.ru/export/v2/catalogue/stock.xml`),
+    ]);
 
-    const xmlStock = responseStock.data;
-    const resultStock = await parseXML(xmlStock);
-    const processedStock = processXMLData(resultStock);
-
-    const jsonStringData = JSON.stringify(processedData, null, 2);
-    const jsonStringStock = JSON.stringify(processedStock, null, 2);
-
-    const jsonData = JSON.parse(jsonStringData);
-    const jsonDataStock = JSON.parse(jsonStringStock);
-
+    // Мерджим данные
     const mergedProductCard = mergePricesWithProductCard(jsonDataStock, jsonData);
 
-    const mergedJsonString = JSON.stringify(mergedProductCard, null, 2);
-
-    item = JSON.parse(mergedJsonString);
+    return mergedProductCard;
   } catch (error) {
     console.error('Ошибка при получении данных из API Gifts.ru', error);
     throw error;
   }
-  return item;
 }
